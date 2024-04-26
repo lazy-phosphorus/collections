9af57cd87142799bd642e0e5c72d3ef9eaa58253
共和国工科生 <1335075427@qq.com>
2024-04-26 09:21:29 +0800

    Initialize

---
+++ src/array.h
@@ -0,0 +1,9 @@
+#ifndef __COLLECTIONS_ARRAY__
+#define __COLLECTIONS_ARRAY__
+
+typedef struct {
+    void* __array;
+    unsigned int Length;
+} Array;
+
+#endif  // __COLLECTIONS_ARRAY__
---
+++ src/test/src/array.c
@@ -0,0 +1,8 @@
+#include "array.h"
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!\n");
+    return 0;
+}
